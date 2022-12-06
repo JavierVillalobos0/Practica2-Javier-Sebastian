0a6b9d19c9c8f9dded8869479874ebc478250714
itsmillz <jorge.millar2601@gmail.com>
2022-12-05 21:19:23 -0300

    se creo la interfaz de inicio de sesion

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/estilos.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <title>Laboratorios</title>
+</head>
+<body>
+
+
+
+    <div class="container-form sign-up">
+        <form action="" class="formulario">
+            <h2 class="create-account">Ingreso Laboratorios</h2>
+            <div class="header">
+                <!-- <h1><img src="/imagenes/ubb.png" alt="" height="150"></h1> -->
+            </div>
+            <div class="data">
+            <input type="email" placeholder="Email" required>
+            <input type="password" placeholder="Contraseña" required name="" id="">
+            </div>
+            <input class="iniciar-sesion-fuente" type="button" value="Iniciar sesión" required name="" id="">
+        </form>
+        <div class="welcome-back">
+         <div class="mesagge">
+            <h2 class="bienvenido">Bienvenido, es un gusto tenerte aquí</h2>
+            <h2><img src="/imagenes/ubbw.png" alt="" height="260" alt=""></h2>
+            <div class="iconos">
+                <div class="border-icon">
+                    <a class="instagram" href="https://www.instagram.com/ubiobio/"><i class='bx bxl-instagram'></i></a>
+                </div>
+                <div class="border-icon">
+                    <a class="linkedin" href="https://www.linkedin.com/school/ubiobio/"><i class='bx bxl-linkedin'></i></a>
+                </div>
+                <div class="border-icon">
+                    <a class="pag-web-ubb" href="https://www.ubiobio.cl/w/"><i class='bx bxs-school'></i></a>
+                </div>
+            </div>
+         </div>
+        </div>
+
+    </div>
+
+</body>
+</html>
